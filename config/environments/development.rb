27e310098d9cf87ecac4294095ea35dbf0c24d5e
Bjoern <bjoern@bjoern-M50Vn.(none)>
2013-02-13 15:31:05 +0100

    devise integration Teil 2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,5 +33,8 @@ Paymeback::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = 
+
+  # Configure Actionmailer
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
