01e167b8818b31dd41fadc697ef75c88069fe2e3
Bjoern <bjoern@bjoern-M50Vn.(none)>
2013-02-13 15:00:54 +0100

    Erster Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paymeback::Application.config.secret_token = '23093a16ec366a470c4d4e864fa5cfd52f0c5ef57868de51d9cc6b077bfe7e80c2f6710b6d60ae4755e1df47ba2d4876a3db322e4428524aa3f6dbc2a4fb8ee2'
