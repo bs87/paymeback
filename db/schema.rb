a57116f98106ab84cdf5aae1539465a5f2499cf0
Lukas Koopmann <derkoopmann@gmx.de>
2013-02-19 17:16:24 +0100

    Navigation angepasst

--- db/schema.rb
+++ db/schema.rb
@@ -82,16 +82,14 @@ ActiveRecord::Schema.define(:version => 20130219134049) do
     t.datetime "updated_at",                             :null => false
     t.string   "firstname"
     t.string   "lastname"
-    t.string   "user_image_uid"
-    t.string   "user_image_name"
-    t.string   "photo_file_name"
-    t.string   "photo_content_type"
-    t.integer  "photo_file_size"
-    t.datetime "photo_updated_at"
     t.string   "adress"
     t.date     "dateofbirth"
     t.string   "city"
     t.integer  "zip"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
