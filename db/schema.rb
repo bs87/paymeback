ed8c161af97e57ba3bd0421260233fef41f3c3b6
Bjoern <bs650866@fh-muenster.de>
2013-02-22 17:35:11 +0100

    db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130221144450) do
+ActiveRecord::Schema.define(:version => 20130222151523) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -49,6 +49,16 @@ ActiveRecord::Schema.define(:version => 20130221144450) do
     t.datetime "created_at",                    :null => false
     t.datetime "updated_at",                    :null => false
     t.boolean  "accepted",   :default => false
+  end
+
+  create_table "nachrichtens", :force => true do |t|
+    t.integer  "sentby"
+    t.integer  "sentto"
+    t.text     "topic"
+    t.text     "body"
+    t.boolean  "read"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "roles", :force => true do |t|
