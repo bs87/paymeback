1a4eac30b67b4ec3980904a47f0d89c3c64f9360
Bjoern <bs650866@fh-muenster.de>
2013-02-27 00:00:42 +0100

    test

--- app/models/debit.rb
+++ app/models/debit.rb
@@ -4,5 +4,5 @@ attr_accessible :betrag, :datum, :emailcurrentuser, :emailuser2, :gezahlt, :info
 validates :firstname, presence: true
 validates :emailuser2, presence: true
 validates_format_of :emailuser2, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create, :message => "Ein @ und ein . muss enthalten sein"
-validates :betrag, presence: true, :format => { :with => /^\d+??(?:\.\d{0,2})?$/ }, :numericality =>{:greater_than => 0}
+validates :betrag, presence: true 
 end
