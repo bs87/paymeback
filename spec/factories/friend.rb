f649aa17c94097e3520c9b8f552653c5cf5bf72e
cydelius <fl.kopp@gmail.com>
2013-03-01 13:39:17 +0100

    nachrichten tests mit capybarra

--- spec/factories/friend.rb
+++ spec/factories/friend.rb
@@ -6,13 +6,41 @@ FactoryGirl.define do
 end
 
 FactoryGirl.define do
- factory :user do	
- email 'dummy@paymeback.de'
- firstname 'dummy'
- lastname 'dummy'
- zip '48143'
- password 'hackme!!'
- password_confirmation 'hackme!!'
+factory :user, class: 'User' do
+id 10 
+email 'dummy24@paymeback.de'
+firstname 'dummy'
+lastname 'dummy'
+adress 'hafenstrasse 12'
+zip '48143'
+city 'muenster'
+password 'hackme!!'
+password_confirmation 'hackme!!'
+dateofbirth '1980-10-10'
 end
 end
 
+FactoryGirl.define do
+factory :user1, class: 'User' do
+id 11
+email 'dummy124@paymeback.de'
+firstname 'dummy1'
+lastname 'dummy1'
+adress 'hafenstrasse 12'
+zip '48143'
+city 'muenster'
+password 'hackme!!'
+password_confirmation 'hackme!!'
+dateofbirth '1980-10-24'
+end
+end
+
+FactoryGirl.define do
+	factory :nachricht, class: 'Nachrichten' do
+		id 23
+		sentby '11'
+		sentto '10'
+		topic 'test'
+		body 'Hallo Welt'
+	end
+end
---
+++ spec/features/nachrichten_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe 'nachrichten' do
+	before(:each) do
+		@user = FactoryGirl.create(:user)
+		@user2 = FactoryGirl.create(:user1)
+		@nachricht = FactoryGirl.create(:nachricht)
+		visit root_path
+		sign_in (@user)
+		visit nachrichtens_path
+	end
+
+#	it 'test' do
+#		page.should have_content 'Nachrichten'
+#	end
+
+	it 'neue nachricht' do
+		visit friends_path
+		find("#freundefinden").click 
+		first("#profilanzeigen").click 
+		find("#nachrichtsenden").click
+		page.should have_content 'Neue Nachricht'
+	end
+
+	it 'nachricht senden' do
+		visit friends_path
+		find("#freundefinden").click 
+		first("#profilanzeigen").click 
+		find("#nachrichtsenden").click
+		fill_in "topic",with: 'huhu'
+		fill_in "body",with: 'hey, wie gehts?'
+		find("#senden").click
+		page.should have_content 'Willkommen'
+	end
+
+	it 'nachricht beantworten' do
+		visit nachrichtens_path
+		first("#beantworten").click
+		page.should have_content 'Neue Nachricht'
+	end
+
+	it 'nachricht anzeigen' do
+		visit nachrichtens_path
+		first("#ansehen").click
+		page.should have_content 'Nachrichten Details'
+	end
+end
+
