f1728366c303bc8e88367314b1ec754f49cf9caa
cydelius <fl.kopp@gmail.com>
2013-03-01 17:27:16 +0100

    tests erweitert, assoziationen in richtige models verschoben

--- spec/models/nachrichten_spec.rb
+++ spec/models/nachrichten_spec.rb
@@ -42,56 +42,6 @@ describe Nachrichten do
 		n.should be_valid
 	end
 
-
-
-	#Assoziationen testen
-
-	it "user has_many nachrichtens" do
-		user = User.reflect_on_association(:nachrichtens)
-		user.macro.should == :has_many
-	end
-
-	it "user has_many nachrichts" do
-		user = User.reflect_on_association(:nachrichts)
-		user.macro.should == :has_many
-	end
-
-	it "user has_many inverse_nachrichtens" do
-		user = User.reflect_on_association(:inverse_nachrichtens)
-		user.macro.should == :has_many
-	end
-
-	it "user has_many inverse_nachrichts" do
-		user = User.reflect_on_association(:inverse_nachrichts)
-		user.macro.should == :has_many
-	end
-
-	it "user has many friends" do
-		user = User.reflect_on_association(:friends)
-		user.macro.should == :has_many
-	end
-
-	it "user has many inverse_friends" do
-		user = User.reflect_on_association(:inverse_friends)
-		user.macro.should == :has_many
-	end
-
-	it "user has_many friendships" do
-		user = User.reflect_on_association(:friendships)
-		user.macro.should == :has_many
-	end
-
-	it "user has_many inverse_friendships" do
-		user = User.reflect_on_association(:inverse_friendships)
-		user.macro.should == :has_many
-	end
-
-	it "user has_many debits" do
-		user = User.reflect_on_association(:debits)
-		user.macro.should == :has_many
-	end
-
-
 	it "nachrichtens belongs_to user" do
 		nachrichten = Nachrichten.reflect_on_association(:user)
 		nachrichten.macro.should == :belongs_to
@@ -101,21 +51,5 @@ describe Nachrichten do
 		nachrichten = Nachrichten.reflect_on_association(:nachricht)
 		nachrichten.macro.should == :belongs_to
 	end
-
-	it "friends belongs_to user" do
-		friend = Friend.reflect_on_association(:user)
-		friend.macro.should == :belongs_to
-	end
-
-	it "friends belongs_to friendship" do
-		friend = Friend.reflect_on_association (:friendship)
-		friend.macro.should == :belongs_to
-	end
-
-	it "debits belongs_to user" do
-		debit = Debit.reflect_on_association (:user)
-		debit.macro.should == :belongs_to
-	end
-
 end
 
